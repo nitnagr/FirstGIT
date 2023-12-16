acf6c24ec7e428d7822b27ee50aeec70bcf88b10
nitnagr <nityanaikwork@gmail.com>
2023-12-16 17:06:08 +0530

    getElementByClassName

---
+++ getElementByClassName.js
@@ -0,0 +1,8 @@
+// Write your code below:
+const fruit=document.getElementsByClassName('fruit');
+fruit[0].style.fontWeight='bold';
+fruit[1].style.fontWeight='bold';
+fruit[2].style.fontWeight='bold';
+fruit[3].style.fontWeight='bold';
+fruit[2].style.backgroundColor='yellow';
+
